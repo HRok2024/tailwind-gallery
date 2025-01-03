ba3c7d8adaccf989aa889b161073ab2f0db6f0c4
rok <rok@gmail.com>
2025-01-03 09:43:17 +0900

    css정리

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div className="text-6xl">시작</div>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="/images/image1.jpg" alt="Photo" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong> 4000
+          </li>
+          <li>
+            <strong>Downloads: </strong> 300
+          </li>
+          <li>
+            <strong>Likes: </strong> 400
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
